cf892df02934e402a9decbb6f2ddb235c823b39c
Michel Jaouen <michel.jaouen@st.com>
2021-09-02 15:44:12 +0200

    platform: stm: b_u585i_iot2a: Active FWU services
    Change-Id: Iba526fd382497746db226d998d819c61a09bee82
    Signed-off-by: Michel Jaouen <michel.jaouen@st.com>

--- platform/ext/target/stm/b_u585i_iot02a/config.cmake
+++ platform/ext/target/stm/b_u585i_iot02a/config.cmake
@@ -9,7 +9,10 @@
 ########################## BL2 #################################################
 
 set(MCUBOOT_IMAGE_NUMBER                2           CACHE STRING    "Whether to combine S and NS into either 1 image, or sign each seperately")
-
+set(MCUBOOT_ALIGN_VAL                   16          CACHE STRING    "align option to build image with imgtool")
+set(MCUBOOT_UPGRADE_STRATEGY            "SWAP"      CACHE STRING    "Upgrade strategy for images")
+set(TFM_PARTITION_FIRMWARE_UPDATE       ON          CACHE BOOL      "Enable firmware update partition")
+set(MCUBOOT_DATA_SHARING                ON          CACHE BOOL      "Enable Data Sharing")
 ################################## Dependencies ################################
 set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL      "Whether to enable the crypto hardware accelerator on supported platforms")
 set(CRYPTO_NV_SEED                      OFF         CACHE BOOL      "Use stored NV seed to provide entropy")
